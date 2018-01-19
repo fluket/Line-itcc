a23fbc3f4909ca4809b41aa81f1588ef369a229b
mis46 <mis46@medicine.local>
2018-01-19 15:35:06 +0700

    ok

--- bot-itcc.php
+++ bot-itcc.php
@@ -53,8 +53,7 @@ if (!is_null($events['events'])) {
 			        'label' => 'View detail',
 			        'uri' => 'http://example.com/page/123',
 			      )
-			    )
-			  )
+			    );
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
